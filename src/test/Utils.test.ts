728755cf05d3fc04a100d908d45ee2b78761e1c5
Sergei Nikiforov <pclp@inbox.ru>
2024-08-18 20:42:01 +0300

    Jest assertions and matchers

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,37 @@
+import { getStringInfo, toUpperCase } from "../app/Utils";
+
+describe('Utils test suite', () => {
+
+    it('should return uppercase of valid string', () => {
+        // arrange:
+        const sut = toUpperCase;
+        const expected = 'ABC';
+
+        // act:
+        const actual = sut('abc');
+
+        // asssert:
+        expect(actual).toBe(expected);
+    })
+
+    it.only('should return info for valid staring', () => {
+        const actual = getStringInfo('My-String');
+
+        expect(actual.lowerCase).toBe('my-string');
+        expect(actual.extraInfo).toEqual({});
+
+        expect(actual.characters.length).toBe(9);
+        expect(actual.characters).toHaveLength(9);
+
+        expect(actual.characters).toEqual(["M", "y", "-", "S", "t", "r", "i", "n", "g"]);
+        expect(actual.characters).toContain<string>('M');
+        expect(actual.characters).toEqual(
+            expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"])
+        )
+
+        expect(actual.extraInfo).not.toBe(undefined);
+        expect(actual.extraInfo).not.toBeUndefined();
+        expect(actual.extraInfo).toBeDefined();
+        expect(actual.extraInfo).toBeTruthy();
+    })
+});
