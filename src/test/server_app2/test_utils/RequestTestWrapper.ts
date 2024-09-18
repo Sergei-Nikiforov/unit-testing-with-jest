6bc950d82e08300af289310695bf9cfb844651e9
Sergei-Nikiforov <pclp@inbox.ru>
2024-09-18 08:58:28 +0300

    Integration tests

---
+++ src/test/server_app2/test_utils/RequestTestWrapper.ts
@@ -0,0 +1,21 @@
+import { HTTP_METHODS } from "../../../app/server_app/model/ServerModel";
+
+export class RequestTestWrapper {
+    public body: object;
+    public method: HTTP_METHODS;
+    public url: string;
+    public headers = {};
+
+    public on(event, cb) {
+        if (event === 'data') return cb(JSON.stringify(this.body));
+
+        cb();
+    }
+
+    public clearFields() {
+        this.body = undefined;
+        this.method = undefined;
+        this.url = undefined;
+        this.headers = {};
+    }
+}
